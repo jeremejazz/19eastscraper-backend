7aab5b214c1bee588cb2d8ba07bc54b9b41e84e2
jeremejazz <jeremejazz15@yahoo.com>
2016-08-18 21:30:34 +0800

    route for api

---
+++ app/controllers/api/schedules.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace controllers\api; 
+/*This class serves the API. Should be accessed by external client*/
+
+class schedules extends \controllers\controller{
+    
+    function getJSON(){
+        echo '{}';
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -4,3 +4,5 @@
 $f3->route('GET /', 'controllers\Home->index');
 $f3->route('GET|POST /@controller', 'controllers\@controller->index');
 $f3->route('GET|POST /@controller/@action', 'controllers\@controller->@action');
+
+$f3->route('GET /api/schedules/data/json', 'controllers\api\schedules->getJSON');
