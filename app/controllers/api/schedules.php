90be35916d19aadd84d8d927af2e72bd26b47acb
jeremejazz <jeremejazz15@yahoo.com>
2016-08-21 23:18:29 +0800

    update filter for blank events

--- app/controllers/api/schedules.php
+++ app/controllers/api/schedules.php
@@ -8,7 +8,7 @@ class schedules extends \controllers\controller{
     function getCurrentSchedules(){
         header('Content-Type: application/json');
         $start = date('Y-m-1');   //ahh can't fint an SQL get current month for now
-        $res = $this->db->exec("SELECT * from schedules where event_date >= ? ", array($start));
+        $res = $this->db->exec("SELECT * from schedules where event_date >= ? and event_gig <> ''", array($start));
         echo json_encode($res);
     }
 
